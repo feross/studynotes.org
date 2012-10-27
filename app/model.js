4b6d71a40d18ee38cb96d56783b4ee0d38f7690f
Feross Aboukhadijeh <feross@feross.org>
2012-10-27 03:42:42 -0700

    remove sequelize

--- app/model.js
+++ app/model.js
@@ -1,86 +1,86 @@
-var Sequelize = require('sequelize');
+// var Sequelize = require('sequelize');
 
 
-// Models
+// // Models
 
-var Course = exports.Course = sequelize.define('Course', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true
-  },
-  desc: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  slug: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false,
-    validate: {
-      is: ['[-a-z]'] // only lower case letters and dashes
-    }
-  }
-});
+// var Course = exports.Course = sequelize.define('Course', {
+//   title: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//     unique: true
+//   },
+//   desc: {
+//     type: Sequelize.TEXT,
+//     allowNull: false
+//   },
+//   slug: {
+//     type: Sequelize.STRING,
+//     unique: true,
+//     allowNull: false,
+//     validate: {
+//       is: ['[-a-z]'] // only lower case letters and dashes
+//     }
+//   }
+// });
 
-var NoteType = exports.NoteType = sequelize.define('NoteType', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true
-  },
-  desc: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  slug: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false,
-    validate: {
-      is: ['[-a-z]'] // only lower case letters and dashes
-    }
-  }
-});
+// var NoteType = exports.NoteType = sequelize.define('NoteType', {
+//   title: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//     unique: true
+//   },
+//   desc: {
+//     type: Sequelize.TEXT,
+//     allowNull: false
+//   },
+//   slug: {
+//     type: Sequelize.STRING,
+//     unique: true,
+//     allowNull: false,
+//     validate: {
+//       is: ['[-a-z]'] // only lower case letters and dashes
+//     }
+//   }
+// });
 
-var Note = exports.Note = sequelize.define('Note', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true
-  },
-  body: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  slug: {
-    type: Sequelize.STRING,
-    // TODO: enforce unqiueness for each course and notetype combo
-    allowNull: false,
-    validate: {
-      is: ['[-a-z]'] // only lower case letters and dashes
-    }
-  }
-});
+// var Note = exports.Note = sequelize.define('Note', {
+//   title: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//     unique: true
+//   },
+//   body: {
+//     type: Sequelize.TEXT,
+//     allowNull: false
+//   },
+//   slug: {
+//     type: Sequelize.STRING,
+//     // TODO: enforce unqiueness for each course and notetype combo
+//     allowNull: false,
+//     validate: {
+//       is: ['[-a-z]'] // only lower case letters and dashes
+//     }
+//   }
+// });
 
-var Schema = exports.Schema = sequelize.define('Schema', {
-  version: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    "default": 0
-  }
-}, {
-  freezeTableName: true
-});
+// var Schema = exports.Schema = sequelize.define('Schema', {
+//   version: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//     "default": 0
+//   }
+// }, {
+//   freezeTableName: true
+// });
 
 
-// Associations
+// // Associations
 
-Course.hasMany(NoteType);
-Course.hasMany(Note);
+// Course.hasMany(NoteType);
+// Course.hasMany(Note);
 
-// NoteType.hasOne(Course);
-NoteType.hasMany(Note);
+// // NoteType.hasOne(Course);
+// NoteType.hasMany(Note);
 
-// Note.hasOne(Course);
-// Note.hasOne(NoteType);
+// // Note.hasOne(Course);
+// // Note.hasOne(NoteType);
