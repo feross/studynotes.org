137a30000846b17266d2a3e6082da1c31d94471d
Feross Aboukhadijeh <feross@feross.org>
2012-10-29 19:11:28 -0700

    Add mongodb and models

--- app/db.js
+++
@@ -1,5 +0,0 @@
-// Re-import these global includes locally so this file can be included from the REPL
-var config = require('./config')
-  , async = require('async')
-  , _ = require('underscore');
-
