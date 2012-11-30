c40ae20c80b8454101a1be0280877c2fb518bd4a
Feross Aboukhadijeh <feross@feross.org>
2012-11-30 02:49:06 -0800

    utils.js -> util.js

--- app/util.js
+++ app/util.js
@@ -6,6 +6,11 @@ module.exports = {
       .replace(/[^-a-z0-9 ]/g, '')
       .replace(/\s+/g, '-');
     return u.str.trim(str, '-');
+  },
+
+  titleify: function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    return args.join(' - ');
   }
 
 };
