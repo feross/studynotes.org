e525431472a846e4a3a59c8f27905f5901213568
Feross Aboukhadijeh <feross@feross.org>
2012-12-20 03:59:56 -0800

    add placeholder support in ie (using modernizr + jquery.placeholder)

--- static/js/client.js
+++ static/js/client.js
@@ -1,43 +1,72 @@
-// Logging functions for convenience
+;(function() {
+
+// Avoid `console` errors in browsers that lack a console.
+var method
+  , noop = function noop() {}
+  , methods = [
+  'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+  'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+  'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+  'timeStamp', 'trace', 'warn']
+  , length = methods.length
+  , console = (window.console = window.console || {})
+
+while (length--) {
+  method = methods[length]
+
+  // Only stub undefined methods.
+  if (!console[method]) {
+    console[method] = noop
+  }
+}
+
+// Short-named logging functions for convenience
 window.log = function() {
-  var args = Array.prototype.slice.call(arguments, 0);
-  console.log.apply(console, args);
-};
+  var args = Array.prototype.slice.call(arguments, 0)
+  if (console.log.apply) console.log.apply(console, args)
+}
 window.error = function() {
-  var args = Array.prototype.slice.call(arguments, 0);
-  args.unshift('ERROR:');
-  console.error.apply(console, args);
-};
-window.u = _;
+  var args = Array.prototype.slice.call(arguments, 0)
+  args.unshift('ERROR:')
+  if (console.error.apply) console.error.apply(console, args)
+}
+
+}())
+
+
+
+;(function() {
+
+window.u = _
 
 // Set search bar's width so it fills the header correctly.
 // Need to ensure this gets called after Typekit fonts are loaded.
 var $headerLeft = $('.header .left')
   , $headerRight = $('.header .right')
-  , $headerSearch = $('.header .search');
+  , $headerSearch = $('.header .search')
 
 function updateHeaderSearchWidth() {
   var headerLeftWidth = $headerLeft.width()
-    , headerRightWidth = $headerRight.width();
+    , headerRightWidth = $headerRight.width()
   $headerSearch
     .css({
       'margin-left': headerLeftWidth,
       'margin-right': headerRightWidth
     })
-    .removeClass('off');
+    .removeClass('off')
 
   // Continue to set the width every 100ms until fonts are done loading.
   // If fonts don't load, then wf-loading gets removed automatically
   // after 1000ms, so this won't run forever. 
   if ($('html').hasClass('wf-loading')) {
-    setTimeout(updateHeaderSearchWidth, 100);
+    setTimeout(updateHeaderSearchWidth, 100)
   }
 }
 
 // Show or hide the browse menu
 function toggleBrowseMenu(_switch) {
-  $('.browse').toggleClass('on', _switch);
-  $('.header .courses').toggleClass('on', _switch);
+  $('.browse').toggleClass('on', _switch)
+  $('.header .courses').toggleClass('on', _switch)
 }
 
 // On DOM ready
@@ -46,53 +75,65 @@ $(function() {
   // Browse menu dropdown
   $('.header .courses').on('click', function (e) {
     // Only handle left-clicks
-    if (e.which != 1) return;
+    if (e.which != 1) return
     
-    toggleBrowseMenu();
-    e.preventDefault();
-  });
+    toggleBrowseMenu()
+    e.preventDefault()
+  })
 
   // Close browse menu on search focus
   $headerSearch.on('focusin', function (e) {
-    toggleBrowseMenu(false);
-  });
+    toggleBrowseMenu(false)
+  })
 
   var $hero = $('.hero, .heroMini')
     , $html = $('html')    
     , $header = $('.header')
-    , $window = $(window);
+    , $window = $(window)
 
   var heroBottom = $hero.length
     ? $hero.offset().top + $hero.height() - $header.height()
-    : undefined;
+    : undefined
 
   // Close browse menu on page scroll
   $(window).on('scroll', u.throttle(function() {
-    toggleBrowseMenu(false);
+    toggleBrowseMenu(false)
 
     // Toggle header text color
     if ($hero.length) {
       var windowScrollTop = $window.scrollTop()
         , className = 'solidHeader'
-        , htmlHasClass = $html.hasClass(className);
+        , htmlHasClass = $html.hasClass(className)
 
       if (htmlHasClass && windowScrollTop < heroBottom) {
-        $html.removeClass(className);
+        $html.removeClass(className)
       } else if (!htmlHasClass && windowScrollTop >= heroBottom) {
-        $html.addClass(className);
+        $html.addClass(className)
       }
     
     }
-  }, 100));
+  }, 100))
 
-  updateHeaderSearchWidth();
+  updateHeaderSearchWidth()
 
   // Page-specific JS
   // if ($('body').hasClass('course'))
 
-});
+  // Load polyfills for old browsers
+  Modernizr.load({
+    test: Modernizr.placeholder,
+    nope: '/js/lib/jquery.placeholder.min.js',
+    callback: function(url, result, key) {
+      if (!result) $('input, textarea').placeholder()
+      log('hey')
+    }
+  })
+
+})
 
 // On DOM load
 $(window).load(function() {
 
-});
+})
+
+}())
