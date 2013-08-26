197ecd6e8d3d0f5d7d097fd6ba8068c2b5dcf5b6
Feross Aboukhadijeh <feross@feross.org>
2013-08-26 00:10:03 -0700

    add cat to 404

--- routes/error.js
+++ routes/error.js
@@ -4,8 +4,12 @@ var express = require('express')
 module.exports = function (app) {
   // 404 page
   app.use(function(req, res) {
-    res.render('404', {
-      title: '404: Page not Found'
+    res.render('not-found', {
+      title: '404: Page Not Found',
+      hero: {
+        title: 'Cat Not Found',
+        desc: '(404 Page Not Found)'
+      }
     })
   })
 
