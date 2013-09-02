4a3a4e8ec217cf41aa2a0cf8ba75a03db47508d0
Feross Aboukhadijeh <feross@feross.org>
2013-09-02 01:13:44 -0700

    handle 500 error in node

--- routes/error.js
+++ routes/error.js
@@ -2,15 +2,35 @@ var config = require('../config')
 var express = require('express')
 
 module.exports = function () {
-  // 404 page
   app.use(function(req, res) {
-    res.render('not-found', {
-      title: '404: Page Not Found',
+    res.render('error', {
+      title: '404 Page Not Found',
       hero: {
         title: 'Cat Not Found',
-        desc: '(404 Page Not Found)'
+        desc: '404 Page Not Found'
       }
     })
   })
 
+  app.get('/500', function (req, res, next) {
+    next(new Error('Manually visited /500'))
+  })
+
+  app.use(function (err, req, res, next) {
+    if (config.isProd) {
+      // Log the exception
+      console.error(err)
+
+      res.render('error', {
+        title: 'Internal Server Error',
+        hero: {
+          title: 'Interal Cat Error',
+          desc: '500 Internal Server Error'
+        }
+      })
+    } else {
+      // Show stack trace during development
+      next(err)
+    }
+  })
 }
