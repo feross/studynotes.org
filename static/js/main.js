6c8e14bd7a1d66f8db3426dd481a15595c6294e5
Feross Aboukhadijeh <feross@feross.org>
2013-09-16 11:12:59 -0700

    Move FB SDK to inline script (for faster load)

--- static/js/main.js
+++ static/js/main.js
@@ -76,38 +76,6 @@ function toggleBrowseMenu (_switch) {
 }
 
 
-/**
- * Executed when the Facebook SDK is ready.
- */
-window.fbAsyncInit = function () {
-  // init the FB JS SDK
-  FB.init({
-    appId      : '466476846723021', // App ID from the App Dashboard
-    channelUrl : '//www.apstudynotes.org/channel.html', // Channel File for x-domain communication
-    status     : true, // check the login status upon init?
-    cookie     : true, // set sessions cookies to allow your server to access the session?
-    xfbml      : true  // parse XFBML tags on this page?
-  })
-
-  // Additional initialization code such as adding Event Listeners goes here
-
-}
-
-
-/**
- * Loads the Facebook SDK asynchronously. Calls `window.fbAsyncInit` when done loading.
- */
-function loadFacebookSDK () {
-  (function (d) {
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0]
-     if (d.getElementById(id)) {return}
-     js = d.createElement('script'); js.id = id; js.async = true
-     js.src = "//connect.facebook.net/en_US/all.js"
-     ref.parentNode.insertBefore(js, ref)
-   }(document))
-}
-loadFacebookSDK()
-
 function isPhone () {
   return $window.width() < 800
 }
