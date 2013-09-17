8bcbc1afcd5df2a002fd7ccaf228be212fb6dd65
Feross Aboukhadijeh <feross@feross.org>
2013-09-17 01:19:21 -0700

    submit page: add rudimentary college picker

--- routes/submit.js
+++ routes/submit.js
@@ -1,8 +1,11 @@
+var _ = require('underscore')
 var auth = require('../auth')
 
 module.exports = function () {
   app.get('/submit/essay', auth.ensureAuth, function (req, res, next) {
+    var colleges = _(app.cache.colleges).flatten()
     res.render('submit', {
+      colleges: colleges,
       hero: {
         title: 'Submit an essay',
         desc: 'Only college admissions essays (for now)'
@@ -11,5 +14,9 @@ module.exports = function () {
       url: '/submit/essay',
       searchFocus: false
     })
+  })
+
+  app.post('/submit/essay', auth.ensureAuth, function (req, res, next) {
+
   })
 }
