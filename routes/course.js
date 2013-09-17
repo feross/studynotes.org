59213836feef3501015d4a455dd6a44ebe21a9a9
Feross Aboukhadijeh <feross@feross.org>
2013-09-16 21:39:01 -0700

    move /courses route to course.js

--- routes/course.js
+++ routes/course.js
@@ -1,6 +1,21 @@
+var _ = require('underscore')
 var model = require('../model')
 
 module.exports = function () {
+  app.get('/courses', function (req, res, next) {
+    var courses = _.flatten(app.cache.courses)
+    res.render('courses', {
+      ads: true,
+      courses: courses,
+      title: 'AP Courses',
+      url: '/courses',
+      hero: {
+        title: 'AP Courses',
+        desc: 'Which AP class do you want to study for?'
+      }
+    })
+  })
+
   app.get('/:courseSlug', function (req, res, next) {
     var courseSlug = req.params.courseSlug
 
