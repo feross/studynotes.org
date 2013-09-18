bbc001b693d8d737ba5a4297d228d491e4e2af23
Feross Aboukhadijeh <feross@feross.org>
2013-09-18 00:09:43 -0700

    Add essay view, model, and route

---
+++ model/Essay.js
@@ -0,0 +1,68 @@
+var _ = require('underscore')
+var model = require('./')
+var mongoose = require('mongoose')
+var plugin = require('./plugin')
+var validate = require('mongoose-validator').validate
+
+var Essay = mongoose.Schema({
+  name: {
+    type: String,
+    index: true,
+    validate: [
+      validate({ message: 'Please give your essay a title.' }, 'notEmpty')
+    ]
+  },
+  body: {
+    type: String,
+    validate: [
+      validate({ message: 'It looks like you forgot to include the actual essay.' }, 'notEmpty')
+    ]
+  },
+  collegeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'College',
+    index: true,
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  },
+  slug: model.SLUG_UNIQUE
+})
+
+// Trim whitespace
+Essay.pre('save', function (next) {
+  var essay = this
+  essay.name = essay.name.trim()
+  next()
+})
+
+Essay.virtual('url').get(function () {
+  var essay = this
+  var collegeId = this.collegeId.toString()
+  var college = _.find(app.cache.colleges, function (c) {
+    return c.id == collegeId
+  })
+  return '/' + college.slug + '/' + essay.slug + '/'
+})
+
+Essay.virtual('searchDesc').get(function () {
+  var essay = this
+  var collegeId = this.collegeId.toString()
+  var college = _.find(app.cache.colleges, function (c) {
+    return c.id == collegeId
+  })
+
+  return college.shortName + ' Admissions Essay'
+})
+
+Essay.plugin(plugin.modifyDate)
+Essay.plugin(plugin.createDate)
+Essay.plugin(plugin.absoluteUrl)
+Essay.plugin(plugin.slug, { model: 'Essay' })
+Essay.plugin(plugin.hits)
+
+module.exports = mongoose.model('Essay', Essay)
---
+++ routes/essay.js
@@ -0,0 +1,30 @@
+var _ = require('underscore')
+var model = require('../model')
+
+module.exports = function () {
+  app.get('/:collegeSlug/:essaySlug', function (req, res, next) {
+    var collegeSlug = req.params.collegeSlug
+    var essaySlug = req.params.essaySlug
+
+    var college = app.cache.colleges[collegeSlug]
+    if (!college) return next()
+
+    model.Essay
+      .findOne({ collegeId: college._id, slug: essaySlug })
+      .exec(function (err, essay) {
+        if (err) return next(err)
+        if (!essay) return next()
+
+        res.render('essay', {
+          ads: true,
+          breadcrumbs: [ college ],
+          college: college,
+          essay: essay,
+          title: essay.name + ' - ' + college.name,
+          url: essay.url
+        })
+
+        essay.hit()
+      })
+  })
+}
