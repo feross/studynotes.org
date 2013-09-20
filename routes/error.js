0b359571b2095aa215bb6a2ccc3ac1eb706bef86
Feross Aboukhadijeh <feross@feross.org>
2013-09-20 12:59:11 -0700

    !!! send correct 404 and 500 status codes with error pages

--- routes/error.js
+++ routes/error.js
@@ -7,11 +7,11 @@ var express = require('express')
 
 module.exports = function () {
   app.use(function(req, res) {
-    res.render('error', {
+    res.status(404).render('error', {
       title: '404 Page Not Found',
       hero: {
-        title: 'Cat Not Found',
-        desc: '404 Page Not Found'
+        title: '404 Page Not Found',
+        desc: 'Sorry about that. Here is a cat for you instead.'
       }
     })
   })
@@ -21,24 +21,19 @@ module.exports = function () {
   })
 
   app.use(function (err, req, res, next) {
-    if (config.isProd || req.url === '/500/') {
-      // Log the exception
-      console.error('EXCEPTION: ' + req.url)
-      console.error('ERROR MESSAGE: ' + err.message || err.toString())
-      console.error(err.stack)
-      console.error('HEADERS:')
-      console.dir(req.headers)
+    // Log the exception
+    console.error('EXCEPTION: ' + req.url)
+    console.dir(err)
+    console.error(err.stack)
+    console.error('HEADERS:')
+    console.dir(req.headers)
 
-      res.render('error', {
-        title: 'Internal Server Error',
-        hero: {
-          title: 'Interal Cat Error',
-          desc: '500 Internal Server Error'
-        }
-      })
-    } else {
-      // Show stack trace during development
-      next(err)
-    }
+    res.status(500).render('error', {
+      title: '500 Internal Server Error',
+      hero: {
+        title: '500 Internal Server Error',
+        desc: 'Sorry about that. Here is a cat for you instead.'
+      }
+    })
   })
 }
