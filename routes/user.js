69c2bc6c2840af6f3b8de9c6c43def31d720ec6f
Feross Aboukhadijeh <feross@feross.org>
2013-09-21 21:42:44 -0700

    Make user profile look better

--- routes/user.js
+++ routes/user.js
@@ -2,26 +2,55 @@
 /*global app */
 "use strict";
 
+var _ = require('underscore')
+var async = require('async')
 var model = require('../model')
 
 module.exports = function () {
   app.get('/user/:userSlug', function (req, res, next) {
     var userSlug = req.params.userSlug
 
-    model.User
-      .findOne({ slug: userSlug })
-      .exec(function (err, theUser) {
-        if (err) return next(err)
-        if (!theUser) return next()
+    async.auto({
+      user: function (cb) {
+        model.User
+          .findOne({ slug: userSlug })
+          .exec(cb)
+      },
+      essays: ['user', function (cb, results) {
+        var user = results.user
+        if (!user) return next()
 
-        res.render('user', {
-          ads: true,
-          theUser: theUser,
-          title: theUser.name + '\'s Notes',
-          url: theUser.url
-        })
+        model.Essay
+          .find({ userId: user._id })
+          .populate('collegeId')
+          .select('-body -prompt')
+          .exec(cb)
+      }]
+    }, function (err, results) {
+      if (err) return next(err)
 
-        theUser.hit()
+      var user = results.user
+      var essays = results.essays
+
+      var totalHits = _(essays).reduce(function (total, essay) {
+        return essay.hits + total
+      }, 0)
+
+      res.render('user', {
+        ads: true,
+        essays: essays,
+        hero: {
+          title: user.name,
+          desc: 'Notes, Essays, and StudyNotes contributions'
+        },
+        title: user.name + '\'s Notes and Essays',
+        totalHits: totalHits,
+        url: user.url,
+        user: user
       })
+
+      user.hit()
+
+    })
   })
 }
