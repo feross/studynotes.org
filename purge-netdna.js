28bbf0916e7a73c5fa82f3ebc05e44cf61b8c81d
Feross Aboukhadijeh <feross@feross.org>
2013-09-23 18:06:41 -0700

    npm install purge-netdna; npm uninstall netdna

--- purge-netdna.js
+++ purge-netdna.js
@@ -1,22 +1,9 @@
 /*jslint node: true */
-/*global app */
 "use strict";
 
-// Script to purge the NetDNA CDN cache
-
-var netdna = require('netdna')
+var purgeNetDNA = require('purge-netdna')
 var secret = require('./secret.js')
 
-netdna = netdna({
-  companyAlias: secret.netdna.companyAlias,
-  consumerKey: secret.netdna.consumerKey,
-  consumerSecret: secret.netdna.consumerSecret
-})
-
-var url = 'zones/pull.json/' + secret.netdna.zoneId + '/cache'
-netdna.delete(url, function (err, response) {
-  if (err || response.code !== 200) {
-    console.error('Response code: ' + response.code)
-    console.dir(err)
-  }
+purgeNetDNA(secret.netdna, function (err) {
+  if (err) throw(err)
 })
