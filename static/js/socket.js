acd6a939bffc80bcb3707b0329d41fd6dfb23b42
Feross Aboukhadijeh <feross@feross.org>
2013-10-07 20:49:56 -0700

    Show online count at top of homepage

--- static/js/socket.js
+++ static/js/socket.js
@@ -36,6 +36,9 @@ function onMessage (str) {
     $('.online')
       .text(message.count + ' ' + studentsStr + ' online ' + pageStr)
       .show()
+    $('.onlineNum')
+      .text(message.count)
+      .show()
   }
 }
 
