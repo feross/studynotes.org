900ca38f5c8b3cfc9b29d22b63b5b3dfee5c210b
Feross Aboukhadijeh <feross@feross.org>
2013-10-07 18:28:40 -0700

    Split sign up into 2 pages

--- routes/signup.js
+++ routes/signup.js
@@ -9,9 +9,11 @@ var email = require('../lib/email')
 var model = require('../model')
 
 module.exports = function (app) {
+  // Page 1
+
   app.get('/signup', auth.returnTo, function (req, res) {
     if (req.user) {
-      res.redirect('/submit/essay/')
+      res.redirect('/')
     } else {
       res.render('signup', {
         errors: req.flash('error'),
@@ -21,14 +23,10 @@ module.exports = function (app) {
   })
 
   app.post('/signup', function (req, res, next) {
-    var college = model.cache.colleges[req.body.college]
     var user = new model.User({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
-      college: college && college._id,
-      collegeMajor: req.body.collegeMajor,
-      collegeYear: req.body.collegeYear
+      password: req.body.password
     })
     user.save(function (err) {
       if (err && err.code === 11000) {
@@ -36,7 +34,7 @@ module.exports = function (app) {
         req.flash('user', req.body)
         res.redirect('/signup/')
       } else if (err && err.name === 'ValidationError') {
-        _(err.errors).map(function (error) {
+        _(err.errors).each(function (error) {
           req.flash('error', error.type)
         })
         req.flash('user', req.body)
@@ -47,16 +45,50 @@ module.exports = function (app) {
         // Automatically login the user
         req.login(user, function (err) {
           if (err) return next(err)
-
-          if (req.session && req.session.returnTo) {
-            var url = req.session.returnTo
-            delete req.session.returnTo
-            res.redirect(url)
-          } else {
-            res.redirect('/signup/')
-          }
+          res.redirect('/signup2/')
           email.notifyAdmin('New user', user)
         })
+      }
+    })
+  })
+
+  // Page 2
+
+  app.get('/signup2', auth.returnTo, function (req, res) {
+    if (!req.user) return res.redirect('/signup/')
+    res.render('signup2', {
+      errors: req.flash('error'),
+      user: req.flash('user')[0]
+    })
+  })
+
+  app.post('/signup2', function (req, res, next) {
+    var user = req.user
+    if (!user) return next(new Error('No logged in user'))
+
+    var college = model.cache.colleges[req.body.college]
+    user.college = college && college.id
+    user.collegeMajor = req.body.collegeMajor
+    user.collegeYear = req.body.collegeYear
+
+    user.save(function (err) {
+      if (err && err.name === 'ValidationError') {
+        _(err.errors).each(function (error) {
+          req.flash('error', error.type)
+        })
+        req.flash('user', req.body)
+        res.redirect('/signup2/')
+      } else if (err) {
+        next(err)
+      } else {
+        if (req.session && req.session.returnTo) {
+          var url = req.session.returnTo
+          delete req.session.returnTo
+          res.redirect(url)
+        } else {
+          res.redirect('/submit/essay/')
+        }
+        email.notifyAdmin('User added details (signup2)', user)
       }
     })
   })
