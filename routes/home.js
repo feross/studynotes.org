0bc59e5c6dce5205501e88bf2ed137a214788a8c
Feross Aboukhadijeh <feross@feross.org>
2013-10-09 04:53:04 -0700

    homepage: Prevent hero desc from "jumping" on live update

--- routes/home.js
+++ routes/home.js
@@ -47,7 +47,7 @@ module.exports = function (app) {
       var locals = {
         hero: {
           title: 'Study Notes',
-          desc: 'Fast, free study guides. Trusted <span class="totalHits">15,000,000</span> times (and counting).',
+          desc: 'Fast, free study guides. Trusted <span class="totalHits">15,000,000</span> times (and&nbsp;counting)',
           descRaw: true,
           image: 'ucla.jpg'
 
