ff38a1c59e4b0f6ace8f1b64fab775706803dcea
Feross Aboukhadijeh <feross@feross.org>
2013-12-29 01:43:35 -0800

    stats: prevent title from being "site under maintenance"

--- liveupdater/index.js
+++ liveupdater/index.js
@@ -260,6 +260,9 @@ LiveUpdater.prototype.getTitle = function (url) {
 
         title = window.$('title').text()
 
+        if (title === 'Site is under maintenance!')
+          return
+
         var index = title.indexOf('- Study Notes')
         if (index !== -1)
           title = title.substring(0, index)
