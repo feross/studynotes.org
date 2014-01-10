e01923e9ec969d2baced2c812a4585bd757852d8
Feross Aboukhadijeh <feross@feross.org>
2014-01-09 19:49:20 -0800

    Fix can css path #131

--- bin/transform-css.js
+++ bin/transform-css.js
@@ -2,5 +2,5 @@ var config = require('../config')
 var fs = require('fs')
 
 var css = fs.readFileSync(process.argv[2], 'utf8')
-css = css.replace(/\.\.\/fonts/g, config.cdnOrigin + '/node_modules/font-awesome/fonts')
+css = css.replace(/\.\.\/fonts/g, config.cdnOrigin + '/font-awesome/fonts')
 fs.writeFileSync(process.argv[2], css)
