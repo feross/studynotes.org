6a11b7b49e81c030c9a4ae4450a1dea3962de3fa
Feross Aboukhadijeh <feross@feross.org>
2014-01-16 01:30:54 -0800

    force ckeditor to always load over https

--- static/js/extra.js
+++ static/js/extra.js
@@ -1,4 +1,4 @@
 var config = require('../../config')
 
-window.CKEDITOR_BASEPATH = config.cdnOrigin + '/ckeditor/'
+window.CKEDITOR_BASEPATH = config.secureOrigin + '/ckeditor/'
 require('ckeditor')
