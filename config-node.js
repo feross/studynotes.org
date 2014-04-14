21ba10c2c1279309b2880c55f597fc383422f9d0
Feross Aboukhadijeh <feross@feross.org>
2014-04-13 21:45:59 -0700

    one less free

--- config-node.js
+++ config-node.js
@@ -15,10 +15,10 @@ exports.out = path.join(exports.root, 'out')
 /**
  * Number of "first click, free" essays that a given session can view before
  * requesting that they subscribe. The user can always view if they have
- * a referer from an external site.
+ * a referrer from an external site.
  * @type {Number}
  */
-exports.numFree = 3
+exports.numFree = 2
 
 /**
  * Price of pro subscription (in cents!)
