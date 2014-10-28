94e000e4d11174a9289559d011b6aa2b18d69df7
Emmanuel Odeke <odeke@ualberta.ca>
2014-10-28 01:56:55 -0600

    forgotpassword: Creating unique reset tokens per request
    Everytime a user requests a new password, any previous resets
    in the backlog are cleared, and a new reset token is created.
    The reset token is affixed to a url mapped to an email, that if
    matching, allows a user to reset their password.

---
+++ model/ResetToken.js
@@ -0,0 +1,18 @@
+var model = require('./')
+var mongoose = require('mongoose')
+var plugin = require('./plugin')
+
+var ResetToken = mongoose.Schema({
+  expiryDate: { type: Date },
+  token: { type: String, required: true},
+  email: { type: String, required: true}
+})
+
+
+ResetToken.plugin(plugin.modifyDate)
+ResetToken.plugin(plugin.createDate)
+ResetToken.plugin(plugin.modifyDate)
+
+ResetToken.plugin(plugin.slug, { model: 'ResetToken' })
+
+module.exports = ResetToken
--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,28 @@ var auth = require('../lib/auth')
 var config = require('../config')
 var passport = require('passport')
 var ssl = require('../lib/ssl')
+var util = require('util')
 var url = require('url')
+var crypto = require('crypto')
+var email = require('../lib/email')
+var model = require('../model')
+
+var FORT_NIGHT_MS = 14 * 24 * 60 * 60 * 1000
+
+var composeResetMsg = function(params) {
+  var from = params.from || '"Study Notes Admin" <admin@apstudynotes.org>'
+  
+  var html = util.format(
+      'Hey %s,<br /><ul />We recently received a password reset from you.<br /><br />' +
+      'Please click <a href="%s/login/resetPassword/%s">here</a> to reset it before: %s',
+      params.name, params.mainURL, params.token, params.expiryDate)
+
+  return {
+    from: from, to: params.to,
+    subject: "Password reset",
+    text: '', html: html
+  }
+}
 
 module.exports = function (app) {
   app.get('/login', ssl.ensureSSL, auth.returnTo, function (req, res) {
@@ -35,10 +56,79 @@ module.exports = function (app) {
     res.send(200) // redirect happens on client-side
   })
 
-  // TODO
-  app.get('/login/forgot', ssl.ensureSSL, function (req, res) {
-    res.send('TODO -- this feature is not ready yet. Email feross@studynotes.org if you forgot your password.')
+  app.get('/login/resetPassword/:resetToken', ssl.ensureSSL, function(req, res, next) {
+    model.ResetToken.findOne({
+      token: req.params.resetToken
+    }, function(err, r) {
+      if (err) return next(err)
+      else if (!r) return res.status(404).send('Invalid or expired token')
+      console.log(req.cookies)
+      res.render('reset-password', {
+        resetToken: r.token,
+        csrftoken: req.cookies.csrftoken
+      })
+    })
   })
-  // app.post('/login/forgot', function (req, res) {
-  // })
+
+  app.post('/login/setPassword/:resetToken/:password', ssl.ensureSSL,
+  function(req, res, next) {
+    console.log('Cest la vie!', req.params, req.body)
+    model.ResetToken.findOneAndRemove({
+      token: req.params.token, expiryDate: {$gt: Date.now()}
+    }, function(err, r) {
+      console.log('token here', req.params)
+      if (err) return next(err)
+      else if (!r) return res.status(404).send('Invalid token')
+
+      model.User.findOne({ email: req.params.email }, function(err, r) {
+        if (err) return next(err)
+        else if (!r) return res.status(404).send('No such user found')
+        r.password = req.params.password // Assumption here is that 'save' method is overwritten
+        r.save(function(err) {
+          if (err) return next(err)
+
+          res.status(200).send('Successfully reset your password')
+        })
+      })
+    })
+  })
+
+  app.get('/login/forgot/:email', ssl.ensureSSL, function (req, res, next) {
+    console.log(req.params.email)
+    model.User.findOne({ email: req.params.email }, 'email name',
+    function(err, u) {
+      console.log(u)
+      if (err) return next(err)
+      else if (!u) return res.status(404).send(false)
+
+      // Make sure all other reset tokens are cleared out
+      model.ResetToken.remove({email: u.email}, function(err) {
+        if (err) return res.redirect('/')
+
+        // Create for a unique token as well as expiry date
+        var rToken = new model.ResetToken({email: u.email})
+        var fHash = crypto.createHash('sha256')
+        fHash.update(rToken._id + 'X' + Math.random() + '^' + rToken._id)
+        rToken.token = fHash.digest('hex')
+        rToken.expiryDate = new Date(Date.now() + FORT_NIGHT_MS)
+
+        // console.log('rToken', rToken, req)
+
+        rToken.save(function(err, savedToken) {
+          if (err) return next(err)
+
+          var msg = composeResetMsg({
+            expiryDate: savedToken.expiryDate,
+            token: savedToken.token, to: u.email,
+            name: u.name, mainURL: req.headers.host
+          })
+
+          email.send(msg, function(err, r) {
+            if (err) return next(err)
+            res.status(200).send("Please check your email address: " + u.email) 
+          })
+        })
+      })
+    })
+  })
 }
