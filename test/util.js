338422f83fd742e9086c9d7c8e1a38f4c9dd75d6
Feross Aboukhadijeh <feross@feross.org>
2015-01-24 16:12:47 -0800

    add test: util.randomBytes

--- test/util.js
+++ test/util.js
@@ -1,6 +1,16 @@
+var util = require('../util')
 var test = require('tape')
 
-test('nothing', function (t) {
-  t.ok(true)
-  t.end()
+test('util.randomBytes', function (t) {
+  t.plan(2)
+
+  util.randomBytes(function (err, bytes) {
+    if (err) throw err
+    t.equal(new Buffer(bytes, 'hex').length, 20, 'default length is 20')
+  })
+
+  util.randomBytes(10, function (err, bytes) {
+    if (err) throw err
+    t.equal(new Buffer(bytes, 'hex').length, 10, 'explicit length')
+  })
 })
