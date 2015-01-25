807fa7374293fdee946d9adc81f6806e68abf450
Feross Aboukhadijeh <feross@feross.org>
2015-01-24 16:02:47 -0800

    use add-commas module

--- util.js
+++ util.js
@@ -1,17 +1,3 @@
 module.exports = exports = require('./util-node')
 
-/**
- * Add commas to an integer, so that it's easier to read.
- * @param {Integer} x The number
- * @return {String} The number with commas
- */
-
-exports.addCommas = function (x) {
-  x += '' // convert to String
-  var rgx = /(\d+)(\d{3})/
-
-  while (rgx.test(x)) {
-    x = x.replace(rgx, '$1' + ',' + '$2')
-  }
-  return x
-}
+exports.addCommas = require('add-commas')
