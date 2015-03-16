a53a8f3db8bbc3150f46f69029a86cd9f8891f5c
Feross Aboukhadijeh <feross@feross.org>
2015-03-16 16:52:11 -0700

    css file should use cdn resources

--- bin/transform-css.js
+++ bin/transform-css.js
@@ -3,6 +3,6 @@
 var fs = require('fs')
 
 var css = fs.readFileSync(process.argv[2], 'utf8')
-css = css.replace(/\.\.\/fonts/g, '/font-awesome/fonts')
-css = css.replace(/select2x2\.png/g, '/select2/select2x2.png')
+css = css.replace(/\.\.\/fonts/g, 'https://cdn.apstudynotes.org/font-awesome/fonts')
+css = css.replace(/select2x2\.png/g, 'https://cdn.apstudynotes.org/select2/select2x2.png')
 fs.writeFileSync(process.argv[2], css)
