7f0ab080bc2536c644de19cbb7a7e88975a7b3d8
Feross Aboukhadijeh <feross@feross.org>
2015-03-16 14:36:54 -0700

    Must build before restart, or MD5 hash will be wrong

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,6 +8,6 @@ sudo supervisorctl stop studynotes-liveupdater
 cd /home/feross/www/studynotes.org && git pull
 cd /home/feross/www/studynotes.org && rm -rf node_modules
 cd /home/feross/www/studynotes.org && npm install --quiet
+cd /home/feross/www/studynotes.org && npm run build
 sudo supervisorctl start studynotes-site
 sudo supervisorctl start studynotes-liveupdater
-cd /home/feross/www/studynotes.org && npm run build
