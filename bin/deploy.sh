b51156c59e65240c64cc07cb0f1893736e065c01
Feross Aboukhadijeh <feross@feross.org>
2015-09-22 22:49:31 -0700

    Zero downtime deploy (fix #146)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,12 +3,26 @@
 set -e
 sudo supervisorctl reload
 sleep 3
+
+if [ -d "/home/feross/www/studynotes.org-build" ]; then
+  echo "ERROR: Build folder already exists. Is another build in progress?"
+  exit 1
+fi
+
+cp -R /home/feross/www/studynotes.org /home/feross/www/studynotes.org-build
+
+cd /home/feross/www/studynotes.org-build && git pull
+cd /home/feross/www/studynotes.org-build && rm -rf node_modules
+cd /home/feross/www/studynotes.org-build && npm install --quiet
+cd /home/feross/www/studynotes.org-build && npm run build
+
 sudo supervisorctl stop studynotes-site
 sudo supervisorctl stop studynotes-liveupdater
-cd /home/feross/www/studynotes.org && git pull
-cd /home/feross/www/studynotes.org && rm -rf node_modules
-cd /home/feross/www/studynotes.org && npm install --quiet
-cd /home/feross/www/studynotes.org && npm run build
+
+cd /home/feross/www && rm -rf studynotes.org
+cd /home/feross/www && mv studynotes.org-build studynotes.org
+
 sudo supervisorctl start studynotes-site
 sudo supervisorctl start studynotes-liveupdater
+
 cd /home/feross/www/studynotes.org && ./bin/purge-maxcdn.js
