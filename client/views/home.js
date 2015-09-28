5a536f279cf68e41774259194fa723597a0fcd3b
Feross Aboukhadijeh <feross@feross.org>
2015-09-28 01:48:30 -0700

    Fade in Twitter widget after onload (fix #226)

--- client/views/home.js
+++ client/views/home.js
@@ -6,6 +6,8 @@ var $window = $(window)
 module.exports = function () {
   // Delay Twitter timeline widget until after onload
   $window.load(function () {
-    loadScript('https://platform.twitter.com/widgets.js')
+    loadScript('https://platform.twitter.com/widgets.js', function () {
+      $('.twitter-home').delay(500).animate({ opacity: 1 }, 200)
+    })
   })
 }
