383cf7013f784d5ad5453982b6855ac6047f66f0
Feross Aboukhadijeh <feross@feross.org>
2015-09-28 02:13:06 -0700

    Don't load twitter widget API on mobile (fix #227)

--- client/views/home.js
+++ client/views/home.js
@@ -6,8 +6,10 @@ var $window = $(window)
 module.exports = function () {
   // Delay Twitter timeline widget until after onload
   $window.load(function () {
-    loadScript('https://platform.twitter.com/widgets.js', function () {
-      $('.twitter-home').delay(500).animate({ opacity: 1 }, 200)
-    })
+    if (!window.isMobile) {
+      loadScript('https://platform.twitter.com/widgets.js', function () {
+        $('.twitter-home').delay(500).animate({ opacity: 1 }, 200)
+      })
+    }
   })
 }
