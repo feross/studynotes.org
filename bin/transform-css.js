ddc0948864dab0ca8f6cda14066c1316ebf2df61
Feross Aboukhadijeh <feross@feross.org>
2015-09-29 02:09:24 -0700

    fix 404 if fontello.svg is fetched

--- bin/transform-css.js
+++ bin/transform-css.js
@@ -5,6 +5,7 @@ var fs = require('fs')
 
 var css = fs.readFileSync(process.argv[2], 'utf8')
 css = css.replace(/select2x2\.png/g, 'https://cdn.apstudynotes.org/select2/select2x2.png')
+css = css.replace(/\.\.\/font/g, 'https://cdn.apstudynotes.org/fontello/font')
 css = new CleanCSS().minify(css).styles
 
 fs.writeFileSync(process.argv[2], css)
