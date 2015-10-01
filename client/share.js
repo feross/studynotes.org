da3ca0e08eecd76e27b0c51beb6f5229d97819ce
Feross Aboukhadijeh <feross@feross.org>
2015-10-01 00:51:33 -0700

    share: name share windows uniquely

--- client/share.js
+++ client/share.js
@@ -1,16 +1,17 @@
 var $ = require('jquery')
 
-var url = encodeURIComponent(window.location.href)
+var url = window.location.href
+var encodedUrl = encodeURIComponent(url)
 
 $('.icon-facebook-squared').on('click', function (e) {
   e.preventDefault()
-  window.open('https://www.facebook.com/sharer/sharer.php?u=' + url, 'sharer', 'width=660,height=620')
+  window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodedUrl, 'facebook-share', 'width=660,height=620')
 })
 
 $('.icon-twitter').on('click', function (e) {
   e.preventDefault()
   var text = encodeURIComponent('Retweet to save a life!')
-  window.open('https://twitter.com/intent/tweet?text=' + text + '&url=' + url + '&original_referer=' + url, 'sharer', 'width=550,height=250')
+  window.open('https://twitter.com/intent/tweet?text=' + text + '&url=' + encodedUrl + '&original_referer=' + encodedUrl, 'twitter-share', 'width=550,height=260')
 })
 
 $('.icon-tumblr').on('click', function (e) {
