366741677a18b214f22ebbdcee3104071108a037
Feross Aboukhadijeh <feross@feross.org>
2015-10-02 22:58:30 -0700

    model: add script for running all pre-save methods

---
+++ update.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+/**
+ * Fetches all user instances and calls user.save() on them.
+ * This is handy for running the pre-save method on all users
+ * when adding new generated fields to the schema.
+ */
+
+var model = require('../model')
+var runParallelLimit = require('run-parallel-limit')
+
+model.connect(function (err) {
+  if (err) throw err
+  model.User
+    .find()
+    .exec(function (err, users) {
+      if (err) throw err
+      console.log(users.length)
+
+      runParallelLimit(users.map(function (user) {
+        return function (cb) {
+          console.log('id', user._id, 'email', user.email, 'name', user.name)
+          user.save(cb)
+        }
+      }), 10, function (err) {
+        if (err) {
+          console.log(err)
+          throw err
+        }
+        console.log('\nDONE')
+      })
+    })
+})
+
