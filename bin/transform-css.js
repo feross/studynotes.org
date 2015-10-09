992270102f4c8ef87f390a7227f3b62d40fd01fe
Feross Aboukhadijeh <feross@feross.org>
2015-10-08 22:56:03 -0700

    Use autoprefixer to remove unnecessary prefixes (fix #260)

--- bin/transform-css.js
+++ bin/transform-css.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 
+var autoprefixer = require('autoprefixer')
 var CleanCSS = require('clean-css')
 var fs = require('fs')
+var postcss = require('postcss')
 
 var css = fs.readFileSync(process.argv[2], 'utf8')
 css = css.replace(/select2x2\.png/g, 'https://cdn.apstudynotes.org/select2/select2x2.png')
 css = css.replace(/\.\.\/font/g, 'https://cdn.apstudynotes.org/fontello/font')
 css = new CleanCSS().minify(css).styles
 
-fs.writeFileSync(process.argv[2], css)
+postcss([ autoprefixer ]).process(css).then(function (result) {
+  result.warnings().forEach(function (warn) {
+    console.warn(warn.toString())
+  })
+  css = result.css
+  fs.writeFileSync(process.argv[2], css)
+})
