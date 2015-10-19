948ac45901c86bba7aaaada61bad05bff61e5e4e
Feross Aboukhadijeh <feross@feross.org>
2015-10-19 10:49:33 -0700

    add config comments

--- config.js
+++ config.js
@@ -1,20 +1,40 @@
 var extend = require('xtend/mutable')
 
+/**
+ * Is site running in production?
+ * @type {Boolean}
+ */
 var isProd = exports.isProd = process.browser
   ? !/^local/.test(window.location.hostname)
   : process.env.NODE_ENV === 'production'
 
+/**
+ * Server listening ports
+ * @type {Object}
+ */
 exports.ports = {
   site: isProd ? 7300 : 4000,
   liveupdater: isProd ? 7301 : 4001
 }
 
+/**
+ * Website hostname
+ * @type {string}
+ */
 exports.siteHost = isProd
   ? 'www.apstudynotes.org'
   : 'localhost:' + exports.ports.site
 
+/**
+ * Origin of the website
+ * @type {string}
+ */
 exports.siteOrigin = (isProd ? 'https' : 'http') + '://' + exports.siteHost
 
+/**
+ * Origin of the CDN server
+ * @type {[type]}
+ */
 exports.cdnOrigin = isProd
   ? 'https://cdn.apstudynotes.org'
   : exports.siteOrigin + '/cdn'
