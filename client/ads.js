e0e12f9c2e8f5daf9fcbd806988ecc6cf6be0188
Feross Aboukhadijeh <feross@feross.org>
2015-10-19 23:32:21 -0700

    add signup upsell throughout site (fix #193)

--- client/ads.js
+++ client/ads.js
@@ -7,6 +7,7 @@ function checkAd () {
   if (replaced || $ad.length === 0 || $ad.html().trim().length) return
 
   var html = '<a class="house" href="/pro/"><div>College Essays that Worked</div></a>'
+  var html = '<a class="house" href="/pro/">College Essays that Worked</a>'
   $($ad.get(0)).replaceWith(html)
 
   replaced = true
