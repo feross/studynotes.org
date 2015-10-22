2f862f38600dc27de6e69ac6a7d851138fda65a1
Feross Aboukhadijeh <feross@feross.org>
2015-10-21 17:36:48 -0700

    temporarily disable house ad

--- client/ads.js
+++ client/ads.js
@@ -1,16 +1,17 @@
-var $ = require('jquery')
+// TEST: disable to see how it effects CPM
+// var $ = require('jquery')
 
-var $ad = $('.adsense-sidebar .adsbygoogle')
-var inserted = false
+// var $ad = $('.adsense-sidebar .adsbygoogle')
+// var inserted = false
 
-function checkAd () {
-  if (inserted || $ad.length === 0 || $ad.html().trim().length) return
+// function checkAd () {
+//   if (inserted || $ad.length === 0 || $ad.html().trim().length) return
 
-  var $firstAd = $ad.parent().eq(0)
-  var $houseAd = $('<div class="adsense-sidebar"><a class="house" href="/pro/">College Essays that Worked</a></div>')
-  $firstAd.after($houseAd)
+//   var $firstAd = $ad.parent().eq(0)
+//   var $houseAd = $('<div class="adsense-sidebar"><a class="house" href="/pro/">College Essays that Worked</a></div>')
+//   $firstAd.after($houseAd)
 
-  inserted = true
-}
+//   inserted = true
+// }
 
-setTimeout(checkAd, 3000)
+// setTimeout(checkAd, 3000)
