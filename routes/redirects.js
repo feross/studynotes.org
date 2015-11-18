e13735af635ff6c8c6f72a2637781827f913e72b
Feross Aboukhadijeh <feross@feross.org>
2015-11-17 16:40:20 -0800

    add grammarly redirect

--- routes/redirects.js
+++ routes/redirects.js
@@ -1,9 +1,13 @@
 module.exports = function (app) {
-  app.get('/amazon', function (req, res) {
+  app.get('/r/amazon', function (req, res) {
     res.redirect(301, 'http://www.amazon.com/ref=as_li_ss_tl?_encoding=UTF8&camp=1789&creative=390957&linkCode=ur2&tag=apstudynotes-20&linkId=BUMOMDOGLYIFKGIC')
   })
 
-  app.get('/essayedge', function (req, res) {
+  app.get('/r/essayedge', function (req, res) {
     res.redirect(301, 'http://www.jdoqocy.com/click-7929089-10856568-1391449830000')
+  })
+
+  app.get('/r/grammarly', function (req, res) {
+    res.redirect(301, 'http://www.dpbolvw.net/click-7929089-11001874-1423611562000')
   })
 }
