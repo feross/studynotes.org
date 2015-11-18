f83f64220dbbe264523c12be4596c15aca9d8b11
Feross Aboukhadijeh <feross@feross.org>
2015-11-17 17:12:07 -0800

    redirect /r to /

--- routes/redirects.js
+++ routes/redirects.js
@@ -1,4 +1,8 @@
 module.exports = function (app) {
+  app.get('/r', function (req, res) {
+    res.redirect('/')
+  })
+
   app.get('/r/amazon', function (req, res) {
     res.redirect(301, 'http://www.amazon.com/ref=as_li_ss_tl?_encoding=UTF8&camp=1789&creative=390957&linkCode=ur2&tag=apstudynotes-20&linkId=BUMOMDOGLYIFKGIC')
   })
