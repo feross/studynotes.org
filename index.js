2f3ef47c511b897633b3cadcd461687b9f46ed2b
Feross Aboukhadijeh <feross@feross.org>
2016-02-15 10:45:55 -0800

    disable opbeat

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = Site
 
 var secret = require('./secret')
-require('opbeat').start(secret.opbeat)
+// require('opbeat').start(secret.opbeat)
 
 var bodyParser = require('body-parser')
 var cluster = require('cluster')
