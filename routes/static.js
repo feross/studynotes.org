c6e56cae8fda8df6dada85de8b609edd983c8a67
Feross Aboukhadijeh <feross@feross.org>
2017-08-24 11:00:20 -0700

    Remove study-guides link
    Amazon aStore is deprecated

--- routes/static.js
+++ routes/static.js
@@ -101,17 +101,6 @@ module.exports = function (app) {
     })
   })
 
-  app.get('/study-guides', function (req, res) {
-    res.render('study-guides', {
-      url: '/study-guides',
-      title: 'Buy Amazon.com AP Study Guides',
-      hero: {
-        title: 'Book Store',
-        desc: 'Get extra help with study guides from Amazon.com'
-      }
-    })
-  })
-
   app.get('/testimonials', function (req, res) {
     res.render('testimonials', {
       url: '/testimonials',
