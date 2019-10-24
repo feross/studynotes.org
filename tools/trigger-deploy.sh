feed658d5983506db561970163fccd743a422322
Feross Aboukhadijeh <feross@feross.org>
2019-10-23 18:15:55 -0700

    Update trigger-deploy.sh

--- tools/trigger-deploy.sh
+++ tools/trigger-deploy.sh
@@ -5,5 +5,5 @@ set -e
 npx figlet "Deploying..."
 git push
 ssh future -t zsh -ci "/home/feross/www/studynotes.org/tools/deploy.sh"
-echo curl https://api.rollbar.com/api/1/deploy/ -F access_token=$(node -p 'require("./secret").rollbar.accessToken') -F environment=production -F revision=$(git log -n 1 --pretty=format:"%H") -F local_username=$(whoami)
+curl https://api.rollbar.com/api/1/deploy/ -F access_token=$(node -p 'require("./secret").rollbar.accessToken') -F environment=production -F revision=$(git log -n 1 --pretty=format:"%H") -F local_username=$(whoami)
 npx figlet "Deployed!"
