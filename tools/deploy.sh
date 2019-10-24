ebd8045600f7fd185de9639a1679cfdec7ab8d63
Feross Aboukhadijeh <feross@feross.org>
2019-10-23 18:58:48 -0700

    Update deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,26 +2,30 @@
 # Update code and restart server (run on server)
 set -e
 
-if [ -d "/home/feross/www/studynotes.org-build" ]; then
+if [ -d "/home/feross/www/build-studynotes.org" ]; then
   echo "ERROR: Build folder already exists. Is another build in progress?"
   exit 1
 fi
 
-cp -R /home/feross/www/studynotes.org /home/feross/www/studynotes.org-build
+if [ -d "/home/feross/www/old-studynotes.org" ]; then
+  echo "ERROR: Old folder exists. Did a previous build crash?"
+  exit 1
+fi
 
-cd /home/feross/www/studynotes.org-build && git pull
-cd /home/feross/www/studynotes.org-build && rm -rf node_modules
-cd /home/feross/www/studynotes.org-build && npm install
-cd /home/feross/www/studynotes.org-build && npm run build
-cd /home/feross/www/studynotes.org-build && npm prune --production
+cp -R /home/feross/www/studynotes.org /home/feross/www/build-studynotes.org
+
+cd /home/feross/www/build-studynotes.org && git pull
+cd /home/feross/www/build-studynotes.org && npm install --no-progress
+cd /home/feross/www/build-studynotes.org && npm run build
+cd /home/feross/www/build-studynotes.org && npm prune --production --no-progress
 
 sudo supervisorctl stop studynotes-site:
 sudo supervisorctl stop studynotes-liveupdater
 
-cd /home/feross/www && mv studynotes.org studynotes.org-old
-cd /home/feross/www && mv studynotes.org-build studynotes.org
+cd /home/feross/www && mv studynotes.org old-studynotes.org
+cd /home/feross/www && mv build-studynotes.org studynotes.org
 
 sudo supervisorctl start studynotes-site:
 sudo supervisorctl start studynotes-liveupdater
 
-cd /home/feross/www && rm -rf studynotes.org-old
+cd /home/feross/www && rm -rf old-studynotes.org
