ed5fe4fe000168ddd9aae9d2baa3429cd654e95a
Feross Aboukhadijeh <feross@feross.org>
2019-11-06 08:52:56 -0800

    Update deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -15,6 +15,7 @@ fi
 cp -R /home/feross/www/studynotes.org /home/feross/www/build-studynotes.org
 
 cd /home/feross/www/build-studynotes.org && git pull
+cd /home/feross/www/build-studynotes.org && rm -rf node_modules
 cd /home/feross/www/build-studynotes.org && npm install --no-progress
 cd /home/feross/www/build-studynotes.org && npm run build
 cd /home/feross/www/build-studynotes.org && npm prune --production --no-progress
