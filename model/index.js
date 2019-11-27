c4b9e8e17b35eeb5029392d58d5dcf54a766741c
Feross Aboukhadijeh <feross@feross.org>
2019-11-27 10:28:59 -0800

    Fix mongo deprecation warning

--- model/index.js
+++ model/index.js
@@ -49,7 +49,7 @@ exports.connect = function (cb) {
     secret.mongo.host + ':' +
     secret.mongo.port + '/' +
     secret.mongo.database,
-    { poolSize: 20, useNewUrlParser: true }
+    { poolSize: 20, useNewUrlParser: true, useUnifiedTopology: true }
   )
   mongoose.connection.on('error', cb)
   mongoose.connection.on('open', function () {
